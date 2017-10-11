89c43ede29c1495a939e78043e666f3c1a5d028d
Nic Burkinshaw <nicbkw@outlook.com>
2017-10-11 20:28:57 +0100

    Update express.js

--- express.js
+++ express.js
@@ -1,60 +1,98 @@
+/**
+ * This nodeJS app retrieves records from a remote mongodb server
+ * Credentials are stored in config.js
+ * 
+ * @author Nic Burkinshaw
+ * based on work rest=api=express by Azat Marden
+ * @license MIT
+ * 
+ */
+
 var express = require('express'),
   mongoskin = require('mongoskin'),
   bodyParser = require('body-parser')
-  logger = require('morgan')
+logger = require('morgan')
+
+var config = require('./config');
 
 var app = express()
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(logger('dev'))
 
-var db = mongoskin.db('mongodb://@localhost:27017/test', {safe:true})
+var db = mongoskin.db('mongodb://' +
+  config.mongodb.username + ':' +
+  config.mongodb.password + '@' +
+  config.mongodb.hostname + ':' +
+  config.mongodb.port + '/' +
+  config.mongodb.database, {
+    safe: true
+  })
 
-app.param('collectionName', function(req, res, next, collectionName){
+app.param('collectionName', function (req, res, next, collectionName) {
   req.collection = db.collection(collectionName)
   return next()
 })
 
-app.get('/', function(req, res, next) {
+app.get('/', function (req, res, next) {
   res.send('please select a collection, e.g., /collections/messages')
 })
 
-app.get('/collections/:collectionName', function(req, res, next) {
-  req.collection.find({} ,{limit: 10, sort: {'_id': -1}}).toArray(function(e, results){
+app.get('/collections/:collectionName', function (req, res, next) {
+  req.collection.find({}, {
+    limit: 1,
+    sort: {
+      '_id': -1
+    }
+  }).toArray(function (e, results) {
     if (e) return next(e)
     res.send(results)
   })
 })
 
-app.post('/collections/:collectionName', function(req, res, next) {
-  req.collection.insert(req.body, {}, function(e, results){
+app.post('/collections/:collectionName', function (req, res, next) {
+  req.collection.insert(req.body, {}, function (e, results) {
     if (e) return next(e)
     res.send(results)
   })
 })
 
-app.get('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.findById(req.params.id, function(e, result){
+app.get('/collections/:collectionName/:id', function (req, res, next) {
+  req.collection.findById(req.params.id, function (e, result) {
     if (e) return next(e)
     res.send(result)
   })
 })
 
-app.put('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.updateById(req.params.id, {$set: req.body}, {safe: true, multi: false}, function(e, result){
+app.put('/collections/:collectionName/:id', function (req, res, next) {
+  req.collection.updateById(req.params.id, {
+    $set: req.body
+  }, {
+    safe: true,
+    multi: false
+  }, function (e, result) {
     if (e) return next(e)
-    res.send((result === 1) ? {msg:'success'} : {msg: 'error'})
+    res.send((result === 1) ? {
+      msg: 'success'
+    } : {
+      msg: 'error'
+    })
   })
 })
 
-app.delete('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.removeById(req.params.id, function(e, result){
+app.delete('/collections/:collectionName/:id', function (req, res, next) {
+  req.collection.removeById(req.params.id, function (e, result) {
     if (e) return next(e)
-    res.send((result === 1)?{msg: 'success'} : {msg: 'error'})
+    res.send((result === 1) ? {
+      msg: 'success'
+    } : {
+      msg: 'error'
+    })
   })
 })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log('Express server listening on port 3000')
 })
-
